311bc42d7e4d729bd4363634c175ccecb7db521c
Vladimir Khomenko <ha10id@users.noreply.github.com>
2022-01-14 17:50:33 +0300

    test no header

--- src/main/java/com/ha10id/reports/service/DocToPdfService.java
+++ src/main/java/com/ha10id/reports/service/DocToPdfService.java
@@ -5,71 +5,72 @@
  */
 package com.ha10id.reports.service;
 
-import com.itextpdf.io.font.PdfEncodings;
-import com.itextpdf.kernel.font.PdfFont;
-import com.itextpdf.kernel.font.PdfFontFactory;
-import com.itextpdf.kernel.font.PdfFontFactory.EmbeddingStrategy;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.element.Paragraph;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
-import com.itextpdf.layout.Document;
-//import static com.lowagie.text.ElementTags.FONT;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import java.util.Arrays;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import org.springframework.stereotype.Service;
 
 /**
- *
+ * https://www.programmerall.com/article/2047471202/
  * @author ha10id
  */
-
 @Service
 public class DocToPdfService {
-
+       private static final String EXCEL_XLS = "xls"; 
+        private static final String EXCEL_XLSX = "xlsx";  
     public Boolean convert(String src, String dst) throws FileNotFoundException, IOException {
-        String k = null;
-        OutputStream fileForPdf = null;
-        String fileName = "./test.doc";
-        String FONT = "./src/main/resources/font/SFNSText.ttf";
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter("./DocToPdf.pdf"));
-        //Below Code is for .doc file
-        if (fileName.endsWith(".doc")) {
-            HWPFDocument doc = new HWPFDocument(new FileInputStream(
-                    fileName));
-            WordExtractor we = new WordExtractor(doc);
-            k = we.getText();
-//                fileForPdf = new FileOutputStream(new File(
-//                        "./DocToPdf.pdf"));
-//                PdfDocument pdfDoc = new PdfDocument(new PdfWriter("./DocToPdf.pdf"));
-            we.close();
-        } //Below Code for .docx file
-        else if (fileName.endsWith(".docx")) {
-            XWPFDocument docx = new XWPFDocument(new FileInputStream(fileName));
-            // using XWPFWordExtractor Class
-            XWPFWordExtractor we = new XWPFWordExtractor(docx);
-            k = we.getText();
-//                fileForPdf = new FileOutputStream(new File(
-//                        "./DocxToPdf.pdf"));
-//                PdfDocument pdfDoc = new PdfDocument(new PdfWriter("./DocToPdf.pdf"));
-            we.close();
-        }
-        PdfFont f1 = PdfFontFactory.createFont(FONT, PdfEncodings.IDENTITY_H);
-        PdfFont f2 = PdfFontFactory.createFont(FONT, PdfEncodings.CP1250, EmbeddingStrategy.PREFER_EMBEDDED);
-        try (Document document = new Document(pdfDoc)) {
+        byte[] pdfBytes = null;
+        try {
+            ByteArrayOutputStream pdfOutStream = new ByteArrayOutputStream();
+            String fileName = "./test.xls";
+            InputStream inputStream = new FileInputStream(fileName);
+            HSSFWorkbook excelBook = new HSSFWorkbook();
+            //Judge the Excel file to convert the 07+ version to the 03 version
+            if(fileName.endsWith(EXCEL_XLS)){  //Excel 2003 
+                excelBook = new HSSFWorkbook(inputStream); 
+            }
+            else if(fileName.endsWith(EXCEL_XLSX)){  // Excel 2007/2010 
+                Transform xls = new Transform();   
+                XSSFWorkbook workbookOld = new XSSFWorkbook(inputStream);
+                xls.transformXSSF(workbookOld, excelBook);
+            }  
 
-            System.out.println(k);
-            document.add(new Paragraph(k).setFont(f1));
-            document.close();
-//            fileForPdf.close();
+//            //convert xls stream to html - w3 document
+//
+//            //convert above w3 document to html input stream
+//            ByteArrayOutputStream htmlOutputStream = new ByteArrayOutputStream();
+//            Source xmlSource = new DOMSource(w3Document);
+//            Result outputTarget = new StreamResult(htmlOutputStream);
+//            TransformerFactory.newInstance().newTransformer().transform(xmlSource, outputTarget);
+//            InputStream htmlInputStream = new ByteArrayInputStream(htmlOutputStream.toByteArray());
+
+            //convert html inputstream to pdf out stream
+            ConverterProperties converterProperties = new ConverterProperties();
+            HtmlConverter.convertToPdf(htmlInputStream, pdfOutStream, converterProperties);
+
+            pdfBytes = pdfOutStream.toByteArray();
+            System.out.println(Arrays.toString(pdfBytes));
+            //write to physical pdf file
+            OutputStream out = new FileOutputStream("./toPDF.pdf");
+            out.write(pdfBytes);
+            out.close();
+        } catch (IOException | ParserConfigurationException | TransformerException e) {
+            System.out.println(e);
+            // log exception details and throw custom exception
         }
+
         return true;
 
     }
