d567c75e12847f6b803be006c7b5ed06e1b6f404
Vladimir Khomenko <vkhomenko@xmark.pro>
2022-01-17 23:51:52 +0300

    litle correction

--- src/main/java/com/ha10id/reports/service/DocToPdfService.java
+++ src/main/java/com/ha10id/reports/service/DocToPdfService.java
@@ -6,7 +6,6 @@
 package com.ha10id.reports.service;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,8 +17,10 @@ import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -41,29 +42,27 @@ public class DocToPdfService {
     private static final String EXCEL_XLS = "xls";
     private static final String EXCEL_XLSX = "xlsx";
 
-    public Boolean convert(String src, String dst) throws FileNotFoundException, IOException {
-        byte[] pdfBytes = null;
+    public Boolean convert(String src, String dst) throws IOException {
         try {
             ByteArrayOutputStream pdfOutStream = new ByteArrayOutputStream();
             InputStream inputStream = new FileInputStream(src);
             HSSFWorkbook excelBook = new HSSFWorkbook();
-            Document htmlDocument = null;
             //Judge the Excel file to convert the 07+ version to the 03 version
-            if (src.endsWith(EXCEL_XLS)) {  //Excel 2003 
+            if (src.endsWith(EXCEL_XLS)) {  //Excel 2003
                 excelBook = new HSSFWorkbook(inputStream);
-            } else if (src.endsWith(EXCEL_XLSX)) {  // Excel 2007/2010 
+            } else if (src.endsWith(EXCEL_XLSX)) {  // Excel 2007/2010
                 Transform xls = new Transform();
                 XSSFWorkbook workbookOld = new XSSFWorkbook(inputStream);
                 xls.transformXSSF(workbookOld, excelBook);
             }
             ExcelToHtmlConverter excelToHtmlConverter = new ExcelToHtmlConverter(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
-            //Remove the Excel header line 
+            //Remove the Excel header line
             excelToHtmlConverter.setOutputColumnHeaders(false);
-            //Remove the Excel line number 
+            //Remove the Excel line number
             excelToHtmlConverter.setOutputRowNumbers(false);
             excelToHtmlConverter.processWorkbook(excelBook);
 
-            htmlDocument = excelToHtmlConverter.getDocument();
+            Document htmlDocument = excelToHtmlConverter.getDocument();
             try (ByteArrayOutputStream outStream = new ByteArrayOutputStream()) {
                 //convert xls stream to html - w3 document
                 DOMSource domSource = new DOMSource(htmlDocument);
@@ -77,19 +76,20 @@ public class DocToPdfService {
                 serializer.transform(domSource, streamResult);
 
                 InputStream htmlInputStream = new ByteArrayInputStream(outStream.toByteArray());
-                //convert html inputstream to pdf out stream
+                //convert html input stream to pdf out stream
                 ConverterProperties converterProperties = new ConverterProperties();
                 HtmlConverter.convertToPdf(htmlInputStream, pdfOutStream, converterProperties);
 
-                pdfBytes = pdfOutStream.toByteArray();
+                byte[] pdfBytes = pdfOutStream.toByteArray();
                 System.out.println(Arrays.toString(pdfBytes));
                 try ( //write to physical pdf file
                         OutputStream out = new FileOutputStream(dst)) {
                     out.write(pdfBytes);
                 }
             }
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException | IllegalArgumentException | ParserConfigurationException | TransformerException e) {
+            System.out.println(e.getMessage());
+            return false;
             // log exception details and throw custom exception
         }
         return true;
